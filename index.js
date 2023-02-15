c5f16b9f18cb3700dcfc04d833be98b2bca47167
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2023-02-15 16:03:02 +0100

    Added website description changing depending on the selected language

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Homepage } from "./src/pages/Homepage/Homepage.js";
 import { selector } from "./src/pages/LanguageSelector/LanguageSelector.js";
 import { toggleModal } from "./src/components/helpers.js";
 import Modal from "./src/components/Modal/Modal.js";
+import { websiteDescription } from "./src/components/websiteDescription.js";
 
 const lang = localStorage.getItem("language");
 
@@ -17,7 +18,20 @@ if (!lang || lang == undefined) {
   );
   app.append(languageModal.htmlCode);
   toggleModal(languageModal.htmlCode, app);
+
+  document
+    .querySelector('meta[name="description"]')
+    .setAttribute("content", websiteDescription.fr);
 } else {
+  if (lang == "fr") {
+    document
+      .querySelector('meta[name="description"]')
+      .setAttribute("content", websiteDescription.fr);
+  } else if (lang == "en") {
+    document
+      .querySelector('meta[name="description"]')
+      .setAttribute("content", websiteDescription.en);
+  }
   app.appendChild(Header("/home"));
 }
 app.appendChild(Homepage());
---
+++ src/components/websiteDescription.js
@@ -0,0 +1,4 @@
+export const websiteDescription = {
+  fr: "Développeur web indépendant spécialisé Frontend. Création de site web ou développement d'application web, mon objectif est de vous permettre de développer votre présence en ligne en réalisant des projets adaptés vos besoins.",
+  en: "Freelance web developer specialized in frontend. Creating websites and developing web applications, my goal is to allow you to develop your online presence by creating projects that fits to your needs.",
+};
