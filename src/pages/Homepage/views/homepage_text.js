fe2ac747cbd5942e6f9069de65b475f7e01d71e7
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2023-08-04 11:57:23 +0200

    Changed the homepage text to fit the apprentice change

--- src/pages/Homepage/views/homepage_text.js
+++ src/pages/Homepage/views/homepage_text.js
@@ -10,28 +10,21 @@ const age = Math.abs(birthYear - 1970);
 export const homepageText = {
   fr: {
     homepageIntroName: "Jonathan Binot",
-    homepageIntroJob:
-      "Développeur web Freelance - Spécialisé Frontend - Applications JS",
-    homepageActivityDescription: `Bonjour, je m'appelle Jonathan Binot, développeur web freelance. Mon objectif est d'aider les entreprises à développer leur présence en ligne grâce à la création de sites web et outils web personnalisés selon leurs besoins. <br>
+    homepageIntroJob: "Développeur web et alternant développeur C# / .NET",
+    homepageActivityDescription: `Bonjour, je m'appelle Jonathan Binot, développeur web et actuellement à la recherche d'une alternance en tant que développeur C# / .NET. <br>
       Je dispose d’une expérience de deux ans dans le développement web et plus en comptant le temps dédié à l’apprentissage en autodidacte. J’ai pu réaliser un certain nombre de projets, site vitrine, réseau social d’entreprise, applications et outils en ligne pour les entreprises, en passant par les projets centrés sur l’amélioration du référencement et du SEO de sites web.  <br>  <br>
       
-      Ma priorité et de répondre convenablement aux besoins des personnes faisant appel à mes services en étant à l’écoute des demandes de mes clients. Pour ce faire, je tente de m’organiser au mieux lors d’un projet afin de pouvoir produire un travail de qualité et en adéquation avec les demandes.  <br> <br>
-      
-      Parce que chaque projet est unique, je peux vous proposer de nombreuses options de personnalisation adaptées au votre. Nous abordons tous les points que vous souhaitez voir en place pour votre projet et nous discutons de leur faisabilité. Si nécessaire, je peux vous apporter quelques conseils et propositions en me basant sur mes expériences passées et mon expertise dans le domaine, dans le but de vous proposer un projet qui réponde au mieux à vos besoins. <br> <br>
-      
-      Si vous êtes à la recherche d'un développeur web professionnel, passionné et dévoué pour votre prochain projet, n'hésitez pas à me contacter. Je serais heureux de discuter de vos besoins et de voir comment je peux vous aider à réussir.
+      Passionné par le développement informatique (web, logiciels et jeux vidéo), je suis motivé à développer mes compétences tout en continuant d'apprendre afin d'être en capacité de travailler au mieux sur des projets variés. Je vais prochainement débuter une formation de développeur concepteur logiciel me permettant d'apprendre le langage de Microsoft C# et la plateforme .NET. <br> <br>
+
+      Sur ce site vous trouverez une présentation de mon travail, mon CV ainsi que mon portfolio sur lequel j'ai listé mes réalisations. Pour accéder au menu du site, vous pouvez cliquer sur le bouton flottant en haut à gauche de la page.
       `,
   },
   en: {
     homepageIntroName: "Jonathan Binot",
-    homepageIntroJob: "Web developer - Frontend - JS Apps",
-    homepageActivityDescription: `Hello, my name is Jonathan Binot, I am a freelancer web developer. My goal is to help companies develop their online presence through the creation of websites and web tools customized to their needs. <br>
-    I have two years of experience in web development and more if we include the time dedicated to self-taught learning. I was able to achieve a certain number of projects, ranging from showcase site, corporate social network, applications and online tools for companies, to projects centered on improving the referencing and SEO of websites. <br> <br>
-    
-    My priority is to respond appropriately to the needs of people needing my services by being attentive to the requests of my clients. To do this, I try to organize myself as well as possible during a project in order to be able to produce quality work that meets the demands. <br> <br>
-    
-    Because each project is unique, I can offer you many customization options adapted to yours. We address all the points you want to see in place for your project and we discuss their feasibility. If necessary, I can provide you with some advice and proposals based on my past experiences and my expertise in the field, with the aim of offering you a project that best meets your needs. <br> <br>
-    
-    If you are looking for a professional, passionate and dedicated web developer for your next project, do not hesitate to contact me. I would be happy to discuss your needs and see how I can help you succeed.`,
+    homepageIntroJob: "Web Developer and Apprentice C# / .NET Developer",
+    homepageActivityDescription: `  Passionate about computer development (web, software, and video games), I am motivated to enhance my skills while continuing to learn to be able to work effectively on diverse projects. I will soon begin a software developer training program to learn the Microsoft language C# and the .NET platform. <br> <br>
+
+    On this website, you will find an overview of my work, my resume, and my portfolio where I have listed the projects I have worked on. To access the site menu, you can click on the floating button at the top left of the page.
+    `,
   },
 };
