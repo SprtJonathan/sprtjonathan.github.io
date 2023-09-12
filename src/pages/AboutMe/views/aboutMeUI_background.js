56651626ac31c3b9eed7910497348fca51d0dd05
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2023-09-12 11:43:23 +0200

    Multiple style fixes

--- src/pages/AboutMe/views/aboutMeUI_background.js
+++ src/pages/AboutMe/views/aboutMeUI_background.js
@@ -8,7 +8,7 @@ const aboutMeBackground = `
   <div id="background-2" class="hidden aboutme--background aboutme--background--yelloworange"></div>
   <div id="background-3" class="hidden aboutme--background aboutme--background--greenblue"></div>
   <video id="background-4" class="hidden" autoplay muted loop id="myVideo">
-  <source class="mp4-source" autoplay="true" src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWI3ZHdxdWhkMjJxbnlkdTY3cW8xdWx0eGZld2VrOG95dDExbDE4ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Z5DdstajZxFOqmZ5TF/giphy.gif">
+  <source class="mp4-source" autoplay="true" src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWI3ZHdxdWhkMjJxbnlkdTY3cW8xdWx0eGZld2VrOG95dDExbDE4ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Z5DdstajZxFOqmZ5TF/giphy.mp4">
   </video>
   <div id="background-5" class="aboutme--background" style="background-image: url(${hpBgOne})"></div>
   </div>
